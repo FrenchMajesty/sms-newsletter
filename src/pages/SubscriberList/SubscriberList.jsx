8424e3f1822d6581999ee3fbb8efbafff409f3b2
Verdi Kapuku <v@askmora.io>
2023-02-19 23:51:37 -0600

    Make the SubscriberList page searchable

--- src/pages/SubscriberList/SubscriberList.jsx
+++ src/pages/SubscriberList/SubscriberList.jsx
@@ -14,25 +14,42 @@ import {
   message,
   List,
 } from 'antd';
+import ParseToHTML from 'html-react-parser';
+import { debounce } from 'lodash';
 import moment from 'moment';
 import HeaderBar from '../../components/HeaderBar/HeaderBar';
 import { useNavigate, Link } from 'react-router-dom';
 import { collection, query, orderBy, limit, getDocs } from 'firebase/firestore';
 import { db } from 'lib/firebase';
+import algoliasearch from 'algoliasearch/lite';
+import useAlgolia from 'use-algolia';
+import { InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
+
+const REACT_APP_ALGOLIA_APP_ID = 'F7IPCQJFCH';
+const REACT_APP_ALGOLIA_SEARCH_KEY = '11700f6a95e0d3da5899191bb02bd2fd';
+const REACT_APP_ALGOLIA_INDEX = 'dev_subscriberlist';
 
 const SubscriberList = () => {
   const [loading, setLoading] = React.useState(false);
   const [qLimit, setLimit] = React.useState(15);
   const [data, setData] = React.useState([]);
+  const [searchState, requestDispatch] = useAlgolia(
+    REACT_APP_ALGOLIA_APP_ID,
+    REACT_APP_ALGOLIA_SEARCH_KEY,
+    REACT_APP_ALGOLIA_INDEX,
+  );
+  const { hits, loading: searchLoading, request } = searchState;
+  console.log(searchState);
   const onSearchChange = (e) => {
     // TODO: Debounce this
     triggerSearch(e.target.value);
   };
-  const triggerSearch = (term) => {
-    term = term.trim();
-    if (term.length < 2) return;
-    console.log('run a search:', term);
-  };
+  const triggerSearch = React.useRef(
+    debounce((query) => {
+      query = query.trim();
+      requestDispatch({ query });
+    }, 200),
+  ).current;
   const userAccount = JSON.parse(localStorage.getItem('account') || '{}');
   const fetchSubscribers = React.useCallback(async () => {
     try {
@@ -69,12 +86,22 @@ const SubscriberList = () => {
   const hasMoreToLoad = React.useMemo(() => {
     return userAccount ? userAccount.subscribers_count > data.length : false;
   }, [userAccount, data.length]);
+  const isSearching = request.query && request.query.length > 2;
   const onLoadMore = () => {
     setLimit(qLimit + 20);
+  };
+  const getHighlightedText = (hit, key) => {
+    try {
+      const highlighted = hit._highlightResult[key].value;
+      return ParseToHTML(highlighted);
+    } catch (e) {
+      return hit[key];
+    }
   };
   React.useEffect(() => {
     fetchSubscribers();
   }, [fetchSubscribers]);
+
   const loadMore =
     data.length > 0 && !loading && hasMoreToLoad ? (
       <div
@@ -97,7 +124,7 @@ const SubscriberList = () => {
         <Row gutter={16}>
           <Col span={24}>
             <Input.Search
-              placeholder="Search by name or number"
+              placeholder="Search by name"
               size="large"
               loading={false}
               onChange={onSearchChange}
@@ -110,23 +137,51 @@ const SubscriberList = () => {
             <Button>Add</Button>
           </Link>
         </Space>
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          loading={loading}
-          loadMore={loadMore}
-          className="subscriber-list"
-          renderItem={(item) => (
-            <List.Item>
-              <List.Item.Meta
-                title={<Link to={`/subscribers/${item.uid}`}>{item.name}</Link>}
-                description={`${formatPhoneNumber(
-                  item.phone_number,
-                )} - Added ${moment(item.created_at).format('MMM Do, YYYY')}`}
-              />
-            </List.Item>
-          )}
-        />
+        {isSearching ? (
+          <List
+            itemLayout="horizontal"
+            dataSource={hits}
+            loading={loading}
+            loadMore={loadMore}
+            locale={{ emptyText: 'No results found' }}
+            className="subscriber-list"
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={
+                    <Link to={`/subscribers/${item.uid}`}>
+                      {getHighlightedText(item, 'name')}
+                    </Link>
+                  }
+                  description={`${formatPhoneNumber(
+                    item.phone_number,
+                  )} - Added ${moment(item.created_at).format('MMM Do, YYYY')}`}
+                />
+              </List.Item>
+            )}
+          />
+        ) : (
+          <List
+            itemLayout="horizontal"
+            dataSource={data}
+            loading={loading}
+            loadMore={loadMore}
+            locale={{ emptyText: 'No subscribers found' }}
+            className="subscriber-list"
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={
+                    <Link to={`/subscribers/${item.uid}`}>{item.name}</Link>
+                  }
+                  description={`${formatPhoneNumber(
+                    item.phone_number,
+                  )} - Added ${moment(item.created_at).format('MMM Do, YYYY')}`}
+                />
+              </List.Item>
+            )}
+          />
+        )}
       </Col>
     </div>
   );
